635e6a78c6f217aa63f76e458a3744bfe464473f
The Glass Corner <ksimons@aggies.ncat.edu>
2021-05-23 23:54:09 -0400

    different dimensional arrays

--- num.py
+++ num.py
@@ -8,7 +8,13 @@ arr2 = np.array((4,5,6,7)) # To create an ndarray, we can pass a list,
                          #tuple or any array-like object into the array()
                        # method, and it will be converted into an ndarray:
 
-                       
+
+arr3 =  np.array(42) #0-Dimension array 
+
+arr4 = np.array([1,2,3]) #1-D
+
+arr5 = np.array([1,2,3],[4,5,6]) #2-D
+
 
 
 
